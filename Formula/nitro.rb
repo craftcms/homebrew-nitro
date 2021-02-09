1e4e75b375797f42534469972898242be3c56687
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-09 12:31:03 -0800

    Brew formula update for nitro version 2.0.0-beta.4

--- Formula/nitro.rb
+++ Formula/nitro.rb
@@ -5,21 +5,21 @@
 class Nitro < Formula
   desc "Nitro is a speedy Docker-based local development environment tuned for Craft CMS"
   homepage "https://getnitro.sh/"
-  version "2.0.0-beta.3"
+  version "2.0.0-beta.4"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.0-beta.3/nitro_darwin_x86_64.tar.gz"
-    sha256 "86ddd4afc55b050de570ea8f1fb2278178500de9b940bfcdddf81ab21cb4d14a"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.0-beta.4/nitro_darwin_x86_64.tar.gz"
+    sha256 "e2e5c23b4a80b1d35ce43d4ae8fab7437ab7248ad614109a6580b72212a1c4ed"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.0-beta.3/nitro_linux_x86_64.tar.gz"
-    sha256 "5657b5305c5f87d11a69ceb63c5dfd1dfa1cd8afa6cc9525aca7e3032c999107"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.0-beta.4/nitro_linux_x86_64.tar.gz"
+    sha256 "b663edf3fb3c448a199c3ab0691666dcd7406bd8ffa9635745487adf518ffbc9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.0-beta.3/nitro_linux_arm64.tar.gz"
-    sha256 "a530673872b63083c195a6a0f2b6de0ad80ea15bf659de014bc7d8d9b190d81a"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.0-beta.4/nitro_linux_arm64.tar.gz"
+    sha256 "9f255857adffcf40b5f85a73aff07fe4a21008b2a3e45edebdab2583a8cb18e3"
   end
 
   def install
