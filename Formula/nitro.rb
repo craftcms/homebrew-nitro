04abcaea16f933e6fa2b72c9b6345535b4ce4f4a
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-06 09:09:46 -0700

    Brew formula update for nitro version 2.0.7

--- Formula/nitro.rb
+++ Formula/nitro.rb
@@ -5,29 +5,35 @@
 class Nitro < Formula
   desc "Nitro is a speedy Docker-based local development environment tuned for Craft CMS"
   homepage "https://getnitro.sh/"
-  version "2.0.6"
+  version "2.0.7"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.6/nitro_darwin_x86_64.tar.gz"
-    sha256 "83ad15e6793c80f38f0e7aaf1775c7f450a24d3925a9affc61e419f9b1be9e33"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.7/nitro_darwin_x86_64.tar.gz"
+    sha256 "bcbfe5c0ca8d88d5801f09003341809cf591f1d47a991705ecfd5ebd93cba205"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.6/nitro_darwin_arm64.tar.gz"
-    sha256 "1a8fd6726d79aeb632a5d055952ccb1ba3b409abd9dcd89ee1512443bde4ee4f"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.7/nitro_darwin_arm64.tar.gz"
+    sha256 "ddabf56415309caa935c7361a9dcf46f254900e40ba6f7aa23b38a540f268adc"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.6/nitro_linux_x86_64.tar.gz"
-    sha256 "e6ff6c80398050940e999b0d8c3f5e61be4e3e57e4d0f71eef82c19582162800"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.7/nitro_linux_x86_64.tar.gz"
+    sha256 "604f3aee479289d00a7a202450cb43846d592a2f262a4c25da6e72768cf3187c"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/craftcms/nitro/releases/download/2.0.6/nitro_linux_arm64.tar.gz"
-    sha256 "29304da822675290eb7882c6078d7193bbdff219f9c9fc3085a4536b5d6ba1f3"
+    url "https://github.com/craftcms/nitro/releases/download/2.0.7/nitro_linux_arm64.tar.gz"
+    sha256 "71fb46fb7bba3e652760de29d2e733794b8cc2b05bdfb50ff678e5dafc70f35e"
   end
 
   def install
     bin.install "nitro"
+  end
+
+  def caveats; <<~EOS
+    Initialize Nitro by running
+      nitro init
+  EOS
   end
 
   test do
